b1807ae082a0c9c52291e72852852fe527af761e
Jaro1805 <145300129+Jaro1805@users.noreply.github.com>
2023-09-19 19:54:41 -0400

    Update funciones.py

--- funciones.py
+++ funciones.py
@@ -1,1 +1,14 @@
 import random
+
+def crear_c():
+    chars = 'abcdefghijklmnopqrstuvwxyz123456789/*-+#$%&'
+    clave = ''
+
+    for x in range(16):
+        clave += random.choice(chars)
+    
+    print(f'clave: {clave}')
+    return clave
+
+if __name__=='__main__':
+    crear_c()
